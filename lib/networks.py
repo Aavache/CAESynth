49f073884e025ec4aa49878892a755c67874b9ba
Aavache <aaron.valero@outlook.com>
2020-09-16 18:14:58 +0900

    Adding models for Magnitude to Phase conversion, for both single and multi frame.

--- lib/networks.py
+++ lib/networks.py
@@ -16,7 +16,7 @@ class SkipGanSynthAE(nn.Module):
     def __init__(self, latent_size, in_channels = 2, feat_width = 128, skip_op = 'add'):
         super(SkipGanSynthAE, self).__init__()
         kw = feat_width//64
-        self.skip_op = skip_op     
+        self.skip_op = skip_op
         dec_ch_scale = 2 if self.skip_op == 'concat' else 1
         self.enc_1 = nn.Conv2d(in_channels, 32, (1,1), 1)
         self.enc_2 = nn.Sequential(*net_utils.create_gansynth_block(32, 32, 'enc'))
@@ -245,7 +245,49 @@ class GanSynthAE(nn.Module):
         out = torch.tanh(self.dec_net(latent))
         
         return out, latent
+
+class GanSynthAE_2(nn.Module):
+    ''' 
+    Using 256X256 features
+    '''
+    def __init__(self, latent_size, in_channels = 2, feat_width = 256):
+        super(GanSynthAE_2, self).__init__()
+        kw = feat_width//64
+        enc_layers = []
+        enc_layers += [nn.Conv2d(in_channels, 32, (1,1), 1)]
+        enc_layers += net_utils.create_gansynth_block(32, 32, 'enc')
+        enc_layers += net_utils.create_gansynth_block(32, 64, 'enc')
+        enc_layers += net_utils.create_gansynth_block(64, 128, 'enc')
+        enc_layers += net_utils.create_gansynth_block(128, 256, 'enc')
+        enc_layers += net_utils.create_gansynth_block(256, 256, 'enc')
+        enc_layers += net_utils.create_gansynth_block(256, 256, 'enc')
+        enc_layers += [nn.Conv2d(256, latent_size, (kw,kw), 1)]
+        self.enc_net = nn.Sequential(*enc_layers)
+        self.enc_net_params = self.enc_net.parameters()
+
+        dec_layers = []
+        dec_layers += [nn.Conv2d(latent_size, 256, kernel_size=(kw,kw), stride=1, padding=(kw-1,kw-1), bias=False)]
+        dec_layers += net_utils.create_gansynth_block(256, 256, 'dec')
+        dec_layers += net_utils.create_gansynth_block(256, 256, 'dec')
+        dec_layers += net_utils.create_gansynth_block(256, 128, 'dec')
+        dec_layers += net_utils.create_gansynth_block(128, 64, 'dec')
+        dec_layers += net_utils.create_gansynth_block(64, 32, 'dec')
+        dec_layers += net_utils.create_gansynth_block(32, 32, 'dec')
+        dec_layers += [nn.Conv2d(32, in_channels, kernel_size=(1,1), stride=1)]
+        self.dec_net = nn.Sequential(*dec_layers)
     
+    def encode(self, input):
+        return self.enc_net(input)
+
+    def decode(self, latent, cond=None):
+        return self.dec_net(latent)
+
+    def forward(self, input, cond=None):
+        latent = self.enc_net(input)
+        out = torch.tanh(self.dec_net(latent))
+        
+        return out, latent
+
 class FC1(nn.Module):
     def __init__(self, input_size=1024, output_size=1024):
         super(FC1, self).__init__()
@@ -256,32 +298,35 @@ class FC1(nn.Module):
         net += [nn.Tanh()]
         net += [nn.Linear(1024,1024)]
         net += [nn.Tanh()]
-        net += [nn.Linear(1024,output_size)]
+        net += [nn.Linear(1024, output_size)]
         self.m2p_net = nn.Sequential(*net)
     
     def forward(self, input):
         return self.m2p_net(input)
 
 class ResConv1(nn.Module):
-    def __init__(self, input_size=1024, output_size=1024):
+    def __init__(self):
         super(ResConv1, self).__init__()
-        self.conv1x1_start = nn.Conv1d(1,32, (1,1))
-        self.b1_ly1 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b1_ly2 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b2_ly1 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b2_ly2 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b3_ly1 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b3_ly2 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b4_ly1 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b4_ly2 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b5_ly1 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b5_ly2 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b6_ly1 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.b6_ly2 = nn.Conv1d(32,32, (5,5),(2,2))
-        self.conv1x1_end = nn.Conv1d(32,1, (1,1))
+        self.conv1x1_start = nn.Conv1d(1,32,1)
+        self.b1_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b1_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b2_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b2_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b3_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b3_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b4_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b4_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b5_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b5_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b6_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b6_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.conv1x1_end = nn.Conv1d(32,1,1)
     
     def forward(self, input):
-        h_start = self.conv1x1_start(input)
+        x = torch.unsqueeze(input, dim=1) 
+
+        h_start = self.conv1x1_start(x)
+
         h_b1 = torch.tanh(self.b1_ly1(h_start))
         h_b1 = self.b1_ly2(h_b1) + h_start
 
@@ -301,7 +346,57 @@ class ResConv1(nn.Module):
         h_b6 = self.b6_ly2(h_b6) + h_b5
 
         out = self.conv1x1_end(h_b6)
-        return out
+
+        return torch.squeeze(out, dim=1)
+
+class DisConv1(nn.Module):
+    def __init__(self, in_size):
+        super(DisConv1, self).__init__()
+        self.conv1x1_start = nn.Conv1d(1,32,1)
+        self.b1_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b1_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b2_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b2_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b3_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b3_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b4_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b4_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b5_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b5_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.b6_ly1 = nn.Conv1d(32,32, 5,1,2)
+        self.b6_ly2 = nn.Conv1d(32,32, 5,1,2)
+        self.conv1x1_end = nn.Conv1d(32,1,1)
+
+        self.linear_1 = nn.Linear(in_size,64)
+        self.linear_2 = nn.Linear(64,1)
+    
+    def forward(self, input):
+        x = torch.unsqueeze(input, dim=1) 
+
+        h_start = self.conv1x1_start(x)
+
+        h_b1 = F.leaky_relu(self.b1_ly1(h_start))
+        h_b1 = self.b1_ly2(h_b1) + h_start
+
+        h_b2 = F.leaky_relu(self.b2_ly1(h_b1))
+        h_b2 = self.b2_ly2(h_b2) + h_b1
+
+        h_b3 = F.leaky_relu(self.b3_ly1(h_b2))
+        h_b3 = self.b3_ly2(h_b3) + h_b2
+
+        h_b4 = F.leaky_relu(self.b4_ly1(h_b3))
+        h_b4 = self.b4_ly2(h_b4) + h_b3
+
+        h_b5 = F.leaky_relu(self.b5_ly1(h_b4))
+        h_b5 = self.b5_ly2(h_b5) + h_b4
+
+        h_b6 = F.leaky_relu(self.b6_ly1(h_b5))
+        h_b6 = self.b6_ly2(h_b6) + h_b5
+
+        h = torch.squeeze(self.conv1x1_end(h_b6), dim=1)
+
+        out = F.leaky_relu(self.linear_1(h))
+        return self.linear_2(out)
 
 class GanSynthDisc(nn.Module):
     ''' 
@@ -318,12 +413,72 @@ class GanSynthDisc(nn.Module):
         layers += net_utils.create_gansynth_block(256, 256)
         layers += net_utils.create_gansynth_block(256, 128)
         self.net = nn.Sequential(*layers)
-        self.linear = nn.Linear(4096, 1)
+        self.linear = nn.Linear(2048, 1)
 
     def forward(self, x):
         h = self.net(x)
         out = self.linear(h.view(h.size(0), -1))
         return out
+
+class SkipGanSynthAE_2(nn.Module):
+    ''' 
+    Using 256X256 features and used for Mag2Phase
+    '''
+    def __init__(self, latent_size, in_channels = 2, feat_width = 256, skip_op = 'add'):
+        super(SkipGanSynthAE_2, self).__init__()
+        kw = feat_width//64
+        self.skip_op = skip_op
+        dec_ch_scale = 2 if self.skip_op == 'concat' else 1
+        self.enc_1 = nn.Conv2d(in_channels, 32, (1,1), 1)
+        self.enc_2 = nn.Sequential(*net_utils.create_gansynth_block(32, 32, 'enc'))
+        self.enc_3 = nn.Sequential(*net_utils.create_gansynth_block(32, 64, 'enc'))
+        self.enc_4 = nn.Sequential(*net_utils.create_gansynth_block(64, 128, 'enc'))
+        self.enc_5 = nn.Sequential(*net_utils.create_gansynth_block(128, 256, 'enc'))
+        self.enc_6 = nn.Sequential(*net_utils.create_gansynth_block(256, 256, 'enc'))
+        self.enc_7 = nn.Sequential(*net_utils.create_gansynth_block(256, 256, 'enc'))
+        self.enc_8 = nn.Conv2d(256, latent_size, (kw,kw), 1)
+        self.enc_net_params = list(self.enc_1.parameters()) + list(self.enc_2.parameters()) + \
+                         list(self.enc_3.parameters()) + list(self.enc_4.parameters()) + \
+                         list(self.enc_5.parameters()) + list(self.enc_6.parameters()) + \
+                         list(self.enc_7.parameters()) + list(self.enc_8.parameters())
+
+        self.dec_1 = nn.Conv2d(latent_size, 256, kernel_size=(kw,kw), stride=1, padding=(kw-1,(kw-1)), bias=False)
+        self.dec_2 = nn.Sequential(*net_utils.create_gansynth_block(256*dec_ch_scale, 256, 'dec'))
+        self.dec_3 = nn.Sequential(*net_utils.create_gansynth_block(256*dec_ch_scale, 256, 'dec'))
+        self.dec_4 = nn.Sequential(*net_utils.create_gansynth_block(256*dec_ch_scale, 128, 'dec'))
+        self.dec_5 = nn.Sequential(*net_utils.create_gansynth_block(128*dec_ch_scale, 64, 'dec'))
+        self.dec_6 = nn.Sequential(*net_utils.create_gansynth_block(64*dec_ch_scale, 32, 'dec'))
+        self.dec_7 = nn.Sequential(*net_utils.create_gansynth_block(32*dec_ch_scale, 32, 'dec'))
+        self.dec_8 = nn.Conv2d(32*dec_ch_scale, in_channels, kernel_size=(1,1), stride=1)
+    
+    def encode(self, input):
+        he_1 = self.enc_1(input)
+        he_2 = self.enc_2(he_1)
+        he_3 = self.enc_3(he_2)
+        he_4 = self.enc_4(he_3)
+        he_5 = self.enc_5(he_4)
+        he_6 = self.enc_6(he_5)
+        he_7 = self.enc_7(he_6)
+        latent = self.enc_8(he_7)
+        return latent, [he_1,he_2,he_3,he_4,he_5,he_6,he_7]
+
+    def decode(self, latent, skip, cond=None):
+        hd_1 = self.dec_1(latent)
+        hd_2 = self.dec_2(net_utils.skip_connection(hd_1, skip[6], self.skip_op))
+        hd_3 = self.dec_3(net_utils.skip_connection(hd_2, skip[5], self.skip_op))
+        hd_4 = self.dec_4(net_utils.skip_connection(hd_3, skip[4], self.skip_op))
+        hd_5 = self.dec_5(net_utils.skip_connection(hd_4, skip[3], self.skip_op))
+        hd_6 = self.dec_6(net_utils.skip_connection(hd_5, skip[2], self.skip_op))
+        hd_7 = self.dec_7(net_utils.skip_connection(hd_6, skip[1], self.skip_op))
+        out = self.dec_8(net_utils.skip_connection(hd_7, skip[0], self.skip_op))
+
+        return out
+
+    def forward(self, input, cond=None):
+        latent, skip = self.encode(input)
+        out = self.decode(latent, skip)
+
+        return out, latent
 
 class GrowingGanSynthEnc(nn.Module):
     ''' 
@@ -371,7 +526,7 @@ class GrowingGanSynthEnc(nn.Module):
             elif i > phase_idx:
                 out = conv(out)
         return out
-
+    
 class GrowingGanSynthEnc_2(nn.Module):
     ''' 
     '''
@@ -418,14 +573,15 @@ class GrowingGanSynthEnc_2(nn.Module):
             elif i > phase_idx:
                 out = conv(out)
         return out
-    
+
 class GrowingGanSynthDec(nn.Module):
     ''' 
     '''
-    def __init__(self, latent_size, in_channels = 2, feat_width = 128, config = None):
+    def __init__(self, latent_size, in_channels = 2, feat_width = 128, config = None, out_act=True):
         super(GrowingGanSynthDec, self).__init__()
         kw = feat_width//64
         self.config = config
+        self.out_act = out_act
         self.dec_net = nn.ModuleList([nn.Conv2d(latent_size, 256, kernel_size=(16,kw), stride=1, padding=(15,(kw//2)), bias=False),
                                     net_utils.GANSynthBlock(256, 256, 'dec'),
                                     net_utils.GANSynthBlock(256, 256, 'dec'),
@@ -458,49 +614,11 @@ class GrowingGanSynthDec(nn.Module):
                 elif i != len(self.dec_net)-1: # Stable and not the last layer.
                     out = to_rgb(out)
                 break
-        return out
-
-class GrowingGanSynthDec_2(nn.Module):
-    ''' 
-    '''
-    def __init__(self, latent_size, in_channels = 2, feat_width = 128, config = None):
-        super(GrowingGanSynthDec_2, self).__init__()
-        kw = feat_width//64
-        self.config = config
-        self.dec_net = nn.ModuleList([nn.Conv2d(latent_size, 256, kernel_size=(16,kw), stride=1, padding=(15,(kw//2)), bias=False),
-                                    net_utils.GANSynthBlock_2(256, 256, 'dec'),
-                                    net_utils.GANSynthBlock_2(256, 256, 'dec'),
-                                    net_utils.GANSynthBlock_2(256, 128, 'dec'),
-                                    net_utils.GANSynthBlock_2(128, 64, 'dec'),
-                                    net_utils.GANSynthBlock_2(64, 32, 'dec'),
-                                    net_utils.GANSynthBlock_2(32, 32, 'dec'),
-                                    nn.Conv2d(32, in_channels, kernel_size=(1,1), stride=1)])
-
-        self.to_rgb = nn.ModuleList([nn.Conv2d(256, in_channels, 1), #Each has 3 out channels and kernel size 1x1!
-                                     nn.Conv2d(256, in_channels, 1),
-                                     nn.Conv2d(256, in_channels, 1),
-                                     nn.Conv2d(128, in_channels, 1),
-                                     nn.Conv2d(64, in_channels, 1),
-                                     nn.Conv2d(32, in_channels, 1),
-                                     nn.Conv2d(32, in_channels, 1),
-                                     nn.Conv2d(in_channels, in_channels, 1)])
-
-    def forward(self, input):
-        for i, (conv, to_rgb) in enumerate(zip(self.dec_net, self.to_rgb)):
-            if i == 0:
-                out = conv(input)
-            else:
-                out = conv(out)
-            if i == self.config['phase']:
-                if i > 0 and self.config['status'] == 'fadein':
-                    skip_rgb = to_rgb(out)
-                    out = to_rgb(out)
-                    out = self.config['alpha'] * skip_rgb + (1 - self.config['alpha']) * out # alpha descreses from 1 to 0 
-                elif i != len(self.dec_net)-1: # Stable and not the last layer.
-                    out = to_rgb(out)
-                break
-        return torch.tanh(out)
-
+        # Applying last activation is optional
+        if self.out_act:
+            return torch.tanh(out)
+        else:
+            return out
 
 def instantiate_autoencoder(opt):
     """ Given the options file, it instantiate a Decoder model.
@@ -530,10 +648,6 @@ def instantiate_autoencoder(opt):
         return GanSynthAE(opt['model']['timbre_latent_size'],
                         opt['model']['in_ch'],
                         feat_width= feat_width)
-    #elif opt['model']['ae'] == 'shallowgansynth':
-    #    return ShallowGanSynthAE(opt['model']['timbre_latent_size'],
-    #                    opt['model']['in_ch'],
-    #                    feat_width= feat_width)
     elif opt['model']['ae'] == 'skipcatgansynth_v2':
         return SkipGanSynthAE_v2(opt['model']['timbre_latent_size'],
                         opt['model']['in_ch'],
@@ -560,11 +674,13 @@ def instantiate_decoder(opt):
     if opt['model']['ae'] == 'growgansynth':
         return GrowingGanSynthDec(opt['model']['timbre_latent_size'],
                         opt['model']['in_ch'],
-                        feat_width= feat_width)
+                        feat_width= feat_width,
+                        out_act= False)
     elif opt['model']['ae'] == 'growgansynth2':
-        return GrowingGanSynthDec_2(opt['model']['timbre_latent_size'],
+        return GrowingGanSynthDec(opt['model']['timbre_latent_size'],
                         opt['model']['in_ch'],
-                        feat_width= feat_width)
+                        feat_width= feat_width,
+                        out_act= True)
 
 def instantiate_discriminator(opt):
     """ Given the options file, it instantiate a discriminator model.
@@ -588,6 +704,42 @@ def instantiate_mag2phase(opt):
     if opt['model']['m2p'] == 'fc1':
         return FC1(opt['model']['in_size'],
                     opt['model']['out_size'])
+    elif opt['model']['m2p'] == 'rescv1':
+        return ResConv1()
+    elif opt['model']['m2p'] == 'gansynth':
+        return GanSynthAE_2(512, 1, feat_width= 256)
+    elif opt['model']['m2p'] == 'skipgansynth':
+        return SkipGanSynthAE_2(512, 1, feat_width= 256, skip_op ='concat')
     else:
         raise NotImplementedError
+
+def instantiate_m2p_disc(opt):
+    """ Given the options file, it instantiate a magnitude to phase model.
+    Parameters:s
+        opt (Dictionary): Options.
+    Returns:
+        m2p instance.
+    """ 
+    if opt['model']['m2pdisc'] == 'discv1':
+        return DisConv1(opt['model']['in_size'])
+    elif opt['model']['m2pdisc'] == 'fc1':
+        return FC1(opt['model']['in_size'],
+                output_size=1)
+    elif opt['model']['m2pdisc'] == 'gansynth':
+        return GanSynthDisc(opt['model']['in_ch'])
+    else:
+        raise NotImplementedError
+
+def instantiate_vocoder(opt):
+    """ Given the options file, it instantiate a magnitude to phase model.
+    Parameters:s
+        opt (Dictionary): Options.
+    Returns:
+        m2p instance.
+    """ 
+    if opt['model']['voc'] == 'waveglow':
+        return DisConv1(opt['model']['in_size'])
+    else:
+        raise NotImplementedError
+
 
